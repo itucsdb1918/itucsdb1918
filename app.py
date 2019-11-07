15c23fb9225c8da2eca1235b854b5e2bc4015aec
M. Fatih Öztürk <52439624+cefatihozturk@users.noreply.github.com>
2019-11-07 17:01:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ status = 0
 @app.route('/')
 def homepage():
     global status
-    return render_template('index.html', Status=status, title="Home Page")
+    return render_template('templates/index.html', Status=status, title="Home Page")
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)

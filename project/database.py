0155b89a63d5b8a5b6e29ceb24bf2a3cada3a256
M. Fatih Öztürk <52439624+cefatihozturk@users.noreply.github.com>
2019-11-07 14:15:41 +0300

    UPDATE: user_list and ADD: interchange_event_list

--- project/database.py
+++ project/database.py
@@ -4,34 +4,51 @@ import psycopg2 as dbapi2
 class dbOp:
     def __init__(self):
         pass
-    
+
     def createTables(self):
         with dbapi2.connect(self.config) as connection:
             cursor = connection.cursor()
 
-            # ADMIN USER INFO TABLOSU
-            query = """DROP TABLE IF EXISTS adminUserInfo CASCADE """
+            # user_list
+            query = """DROP TABLE IF EXISTS user_list CASCADE """
             cursor.execute(query)
-            query = """CREATE TABLE adminUserInfo (
-                                                            ID SERIAL PRIMARY KEY,
-                                                            Username VARCHAR(15) NOT NULL UNIQUE,
-                                                            Name VARCHAR(40) NOT NULL,
-                                                            Surname VARCHAR(40) NOT NULL,
-                                                            Mail VARCHAR(100) UNIQUE NOT NULL,
-                                                            SchoolName VARCHAR(100) NOT NULL,
-                                                            CampusName VARCHAR(100),
-                                                            WishlistID SERIAL PRIMARY KEY
-                                                            FOREIGN KEY ID REFERENCES adminInterchangeOps(LenderID) 
-                                                            ON DELETE RESTRICT ON UPDATE CASCADE,
-                                                            FOREIGN KEY ID REFERENCES adminInterchangeOps(BorrowerID) 
-                                                            ON DELETE RESTRICT ON UPDATE CASCADE
+            query = """CREATE TABLE user_list (
+                                                            userId SERIAL PRIMARY KEY,
+                                                            username VARCHAR(15) NOT NULL UNIQUE,
+                                                            name VARCHAR(40) NOT NULL,
+                                                            surname VARCHAR(40) NOT NULL,
+                                                            email VARCHAR(100) UNIQUE NOT NULL,
+                                                            schoolName VARCHAR(100) NOT NULL,
+                                                            campusName VARCHAR(100),
+                                                            wishlistId INTERGER,
+
+                                                            FOREIGN KEY wishlistId REFERENCES wish_list(wishlistId) ON DELETE RESTRICT ON UPDATE CASCADE
                                                                             )"""
             cursor.execute(query)
+
+
+            # interchange_event_list
+            query = """DROP TABLE IF EXISTS interchange_event_list CASCADE """
+            cursor.execute(query)
+            query = """CREATE TABLE interchange_event_list (
+                                                            interchangeId SERIAL PRIMARY KEY,
+                                                            lenderId INTEGER NOT NULL UNIQUE,
+                                                            borrowerId INTEGER NOT NULL UNIQUE,
+                                                            time TIMESTAMP,
+                                                            bookId INTEGER NOT NULL UNIQUE,
+
+
+                                                            FOREIGN KEY bookId REFERENCES book_info_list(bookId) ON DELETE RESTRICT ON UPDATE CASCADE
+
+                                                                            )"""
+                                                            # lenderID = userId
+                                                            # borrowerId = userId
+
+            cursor.execute(query)
+
             
-            
-            
-            
+
         def db_init(self):
             pass
-        
-db = dbOp()            
+
+db = dbOp()

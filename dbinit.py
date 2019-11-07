44837041652059f605880495af2042c9a1d0af7f
M. Fatih Öztürk <52439624+cefatihozturk@users.noreply.github.com>
2019-11-07 15:59:35 +0300

    ADD: user_list and interchange_event_list

--- dbinit.py
+++ dbinit.py
@@ -5,8 +5,34 @@ import psycopg2 as dbapi2
 
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    """
+    CREATE TABLE user_list( 
+                      userId SERIAL PRIMARY KEY,
+                      username VARCHAR(15) NOT NULL UNIQUE,
+                      password VARCHAR(16) NOT NULL,
+                      name VARCHAR(40) NOT NULL,
+                      surname VARCHAR(40) NOT NULL,
+                      email VARCHAR(100) UNIQUE NOT NULL,
+                      schoolName VARCHAR(100) NOT NULL,
+                      campusName VARCHAR(100),
+                      wishlistId INTERGER,
+
+                      FOREIGN KEY wishlistId REFERENCES wish_list(wishlistId) ON DELETE RESTRICT ON UPDATE CASCADE                          
+                     );
+
+    CREATE TABLE interchange_event_list( 
+                     interchangeId SERIAL PRIMARY KEY,
+                     lenderId INTEGER NOT NULL UNIQUE,
+                     borrowerId INTEGER NOT NULL UNIQUE,
+                     time TIMESTAMP,
+                     bookId INTEGER NOT NULL UNIQUE,
+
+                     FOREIGN KEY bookId REFERENCES book_info_list(bookId) ON DELETE RESTRICT ON UPDATE CASCADE
+      
+                     );
+
+    
+    """
 ]
 
 
@@ -24,3 +50,4 @@ if __name__ == "__main__":
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
     initialize(url)
+

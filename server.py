8730180e1cc9be7a879c07ca90f28d68c75eaf26
M. Fatih Öztürk <52439624+cefatihozturk@users.noreply.github.com>
2019-11-08 15:59:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-	return "itucsdb1918 team's project InterBooks' homepage"
-    	#return render_template('layout.html',Status =status,title = "Home Page")
+	#return "itucsdb1918 team's project InterBooks' homepage"
+    	return render_template('index.html')
 
 
 if __name__ == '__main__':

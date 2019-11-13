260da923bffbd29a56a4392b198a08a22df4eab6
Emre Reyhanlioglu <reyhanlioglu@itu.edu.tr>
2019-11-13 14:10:33 +0300

    ADD: User data model

---
+++ model/user.py
@@ -0,0 +1,10 @@
+# USER DATA MODEL CLASS
+
+class User:
+    def __init__(self, userName, password, firstName, lastName, schoolName, campusName):
+        self.userName = userName
+        self.password = password
+        self.firstName = firstName
+        self.lastName = lastName
+        self.schoolName = schoolName
+        self.campusName = campusName

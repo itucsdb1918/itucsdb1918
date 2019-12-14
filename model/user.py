e88066f7fb37692bdfe277d6024cfe96d8d26970
Emre Reyhanlioglu <reyhanlioglu@itu.edu.tr>
2019-12-14 14:53:56 +0300

    UPDATE: User object updated

--- model/user.py
+++ model/user.py
@@ -1,11 +1,51 @@
 # USER DATA MODEL CLASS
 
 class User:
-    def __init__(self, userId, userName, password=None, firstName, lastName, schoolName, campusName):
-        self.userId = userId
-        self.userName = userName
+    def __init__(self):
+        self.id = ""
+        self.username = ""
+        self.email = ""
+        self.password = ""
+        self.firstname = ""
+        self.lastname = ""
+        self.schoolName = ""
+        self.campusName = ""
+        self.wishlistId = ""
+
+    def setId(self, id):
+        self.id = id
+    def setUsername(self,username):
+        self.username = username
+    def setEmail(self,email):
+        self.email = email
+    def setPassword(self,password):
         self.password = password
-        self.firstName = firstName
-        self.lastName = lastName
+    def setFirstname(self,firstname):
+        self.firstname = firstname
+    def setLastname(self,lastname):
+        self.lastname = lastname
+    def setSchoolName(self,schoolName):
         self.schoolName = schoolName
+    def setCampusName(self,campusName):
         self.campusName = campusName
+    def setWishlistId(self,wishlistId):
+        self.wishlistId = wishlistId
+
+    def getId(self):
+        return self.id
+    def getUsername(self):
+        return self.username
+    def getEmail(self):
+        return self.email
+    def getPassword(self):
+        return self.password
+    def getName(self):
+        return self.name
+    def getLastname(self):
+        return self.lastname
+    def getSchoolName(self):
+        return self.schoolName
+    def getCampusName(self):
+        return self.campusName
+    def getWishlistId(self):
+        return self.wishlistId

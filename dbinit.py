14ad37728ce3303017f4fea729a2a0b433a65936
cefatihozturk <mfatihozturk00@gmail.com>
2019-12-15 17:00:52 +0300

    UPDATE: user_list, interchange_event_list, school_list were updated in dbinit.py

--- dbinit.py
+++ dbinit.py
@@ -6,34 +6,17 @@ import psycopg2 as dbapi2
 
 INIT_STATEMENTS = {
     """
-CREATE TABLE IF NOT EXISTS "book_info_list" (
-    "bookid" SERIAL   NOT NULL,
-    "bookname" VARCHAR(40)   NOT NULL,
-    "bookauthor" VARCHAR(40)   NOT NULL,
-    "totalpages" INTEGER   NOT NULL,
-    CONSTRAINT "pk_book_info_list" PRIMARY KEY (
-        "bookname", "bookauthor"
-     )
-);
-
-CREATE TABLE IF NOT EXISTS "wish_list" (
-    "wishlistid" INTEGER NOT NULL,
-    "bookid" INTEGER NOT NULL,
-    CONSTRAINT "pk_wish_list" PRIMARY KEY (
-        "wishlistid","bookid"
-     )
-);
 
 CREATE TABLE IF NOT EXISTS "user_list" (
     "userid" serial   NOT NULL,
-    "username" VARCHAR(15)  NOT NULL,
-    "password" VARCHAR(50)  NOT NULL,
+    "username" VARCHAR(15)   NOT NULL,
+    "password" VARCHAR(50)   NOT NULL,
     "firstname" VARCHAR(40)   NOT NULL,
     "lastname" VARCHAR(40)   NOT NULL,
     "email" VARCHAR(100)   NOT NULL,
-    "schoolname" VARCHAR(100)   NOT NULL,
+    "schoolid" INTEGER   NOT NULL,
     "campusname" VARCHAR(100)   NOT NULL,
-    "wishlistid" SERIAL NOT NULL,
+    "wishlistid" serial   NOT NULL,
     CONSTRAINT "pk_user_list" PRIMARY KEY (
         "userid"
      )
@@ -44,46 +27,40 @@ CREATE TABLE IF NOT EXISTS "interchange_event_list" (
     "lenderid" INTEGER   NOT NULL,
     "borrowerid" INTEGER   NOT NULL,
     "time" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    "bookid" INTEGER   NOT NULL,
+    "bookname" VARCHAR(40)   NOT NULL,
+    "bookauthor" VARCHAR(40)   NOT NULL,
+    "totalpages" INTEGER   NOT NULL,
+    "publisher" VARCHAR(40)   NOT NULL,
     CONSTRAINT "pk_interchange_event_list" PRIMARY KEY (
         "interchangeid"
      )
 );
 
+CREATE TABLE IF NOT EXISTS "school_list" (
+    "schoolid" SERIAL   NOT NULL,
+    "schoolname" VARCHAR(50)   NOT NULL,
+    "schooltype" VARCHAR(40)   NOT NULL,
+    "schoolcountry" VARCHAR(40)   NOT NULL,
+    "schoolcity" VARCHAR(40)   NOT NULL,
+    "schoolphonenumber" VARCHAR(40)   NOT NULL,
+    CONSTRAINT "pk_school_list" PRIMARY KEY (
+        "schoolid"
+     )
+);
 
+
+ALTER TABLE "user_list" ADD CONSTRAINT "fk_user_list_schoolid" FOREIGN KEY("schoolid")
+REFERENCES "school_list" ("schoolid");
+
+ALTER TABLE "interchange_event_list" ADD CONSTRAINT "fk_interchange_event_list_lenderid" FOREIGN KEY("lenderid")
+REFERENCES "user_list" ("userid");
+
+ALTER TABLE "interchange_event_list" ADD CONSTRAINT "fk_interchange_event_list_borrowerid" FOREIGN KEY("borrowerid")
+REFERENCES "user_list" ("userid");
 
     """
 }
 
-"""
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('1984','George Orwell',237);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('The Count of Monte Cristo','Alexandre Dumas',276);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('Animal Farm','George Orwell',141);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('The Brothers Karamazov','Fyodor Dostoyevsky',10);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('The Little Prince','Antoine de Saint-Exupery',10);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('Romeo and Juliet','William Shakespeare',10);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('War and Peace','Leo Tolstoy ',10);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('Robinson Crusoe','Daniel Defoe',10);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('David Copperfield','Charles Dickens',10);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('Anna Karenina','George Eliot',10);
-INSERT INTO book_info_list(bookname,bookauthor, totalpages)VALUES ('The Call of the Wild','Jack London',10);
-
-INSERT INTO user_list (username,password,firstname,lastname, email,schoolname,campusname,wishlistid)
-VALUES ('admin','12345','admin','admin','admin@interbooks.com', 'Istanbul Technical University','Ayazaga',1);
-
-INSERT INTO user_list (username,password,firstname,lastname, email,schoolname,campusname,wishlistid)
-VALUES ('cefatihozturk','0','M. Fatih','Öztürk','fatih@interbooks.com', 'Istanbul Technical University','Ayazaga',2);
-
-INSERT INTO user_list (username,password,firstname,lastname, email,schoolname,campusname,wishlistid)
-VALUES ('reyhanlioglu','1','Emre','Reyhanlıoğlu','emre@interbooks.com', 'Istanbul Technical University','Ayazaga',3);
-
-INSERT INTO wish_list(bookid, wishlistid) VALUES (1, 1);
-INSERT INTO wish_list(bookid, wishlistid) VALUES (4, 1);
-INSERT INTO wish_list(bookid, wishlistid) VALUES (5, 1);
-
-
-INSERT INTO interchange_event_list(lenderid, borrowerid, bookid) VALUES (1, 2,8);
-INSERT INTO interchange_event_list(lenderid, borrowerid, bookid) VALUES (1, 3,5);
 
 """
 
